c1bfae3858dbd4bc42ca0cf4e7683f78fccb1529
bifree <bifree2bfree@gmail.com>
2021-12-30 07:20:42 +0900

    service

---
+++ WordPressTheme/page-service.php
@@ -0,0 +1,155 @@
+<?php get_header(); ?>
+    <main>
+        <div class="service">
+            <div class="service__title">
+                <h1>事業紹介</h1>
+                <div class="border"></div>
+            </div>
+            <div class="sub-contents-title">
+                <h2>虹のように多彩な色でホームページに個性を</h2>
+            </div>
+            <div class="contents-text">
+                <p>Bifreeは2021年2月2日、LGBTQ当事者とその支援者・理解者であるアライを結ぶウェブメディアとしてオープン致しました。その後、そのウェブメディア構築で得た技術を前面に出し、Webページ制作・システム構築などにも活動を広げ、2021年10月1日現在LGBTQ当事者・非当事者だけでなく外国人のメンバーも合わせ12名で活動しております。</p>
+            </div>
+            <div class="contents-item">
+                <div class="item-all">
+                    <div class="item-info">
+                        <div class="item-title">
+                            <div class="number"><h3>01</h3></div>
+                            <div class="title-text"><p>1人に全てを押し付けないチーム制</p></div>
+                        </div>
+                        <div class="item-text">
+                            <p>Bifreeは3・4名以上のチームを組み、どんなに小さなプロジェクトであっても取り組んでいます。メンバーそれぞれの負担を減らす目的だけでなく、IT業界の先輩から後輩へいつでもスキル向上の為の助言ができる環境を実現し、それを用いての品質担保したサービス提供を実現します。</p>
+                        </div>
+                    </div>
+                    <div class="item-image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/teamwork.jpg" alt="teamwork">
+                    </div>
+                </div>
+                <div class="item-all">
+                    <div class="item-info">
+                        <div class="item-title">
+                            <div class="number"><h3>02</h3></div>
+                            <div class="title-text"><p>ワンストップ態勢で無駄を省く</p></div>
+                        </div>
+                        <div class="item-text">
+                            <p>企画・立案、デザイン、コーディング、品質管理などを団体内で行っているため、管理者の目が細部まで届く構造となっています。またお客様のご要望に応じて柔軟にZoomでのオンライン会議などを行い、不安を払拭、その場にそれぞれの担当者を置くことで直接ご要望を反映させます。</p>
+                        </div>
+                    </div>
+                    <div class="item-image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bifree_logo_b.svg" alt="onestep">
+                    </div>
+                </div>
+                <div class="item-all">
+                    <div class="item-info">
+                        <div class="item-title">
+                            <div class="number"><h3>03</h3></div>
+                            <div class="title-text"><p>各々の色を出し、柔軟にニーズに応える</p></div>
+                        </div>
+                        <div class="item-text">
+                            <p>Bifreeは日本人のLGBTQ当事者・非当事者だけでなく、外国人もプログラミングを行うメンバーとして迎え入れており、バックグラウンドの違う者たちによる知見の集大成を1つ1つのプロジェクトでクライアントのニーズに寄り添いながら具体化させています。</p>
+                        </div>
+                    </div>
+                    <div class="item-image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bifree_logo_b.svg" alt="background">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="work">
+            <div class="contents-title">
+                <h1>制作実績</h1>
+                <div class="border"></div>
+            </div>
+            <div class="work-item">
+                <div class="work-site">
+                    <div class="site-image">
+                        <img src="img/bifree逕ｻ蜒・sample1.png" alt="Corporate site">
+                    </div>
+                    <div class="site-title">
+                        <p>Bloom -Flower Arrangement School-</p>
+                        <a href="#">Corporate site</a>
+                    </div>
+                </div>
+                <div class="work-site">
+                    <div class="site-image">
+                        <img src="img/bifree逕ｻ蜒・sample6.png" alt="Corporate site">
+                    </div>
+                    <div class="site-title">
+                        <p>irOdori<br><br></p>
+                        <a href="#">Corporate site</a>
+                    </div>
+                </div>
+                <div class="work-site">
+                    <div class="site-image">
+                        <img src="img/bifree逕ｻ蜒・sample2.png" alt="Brand site">
+                    </div>
+                    <div class="site-title">
+                        <p>STRAD TAILOR<br><br></p>
+                        <a href="#">Brand site</a>
+                    </div>
+                </div>
+                <div class="work-site">
+                    <div class="site-image">
+                        <img src="img/bifree逕ｻ蜒・sample3.png" alt="Hospital site">
+                    </div>
+                    <div class="site-title">
+                        <p>整体院 神威<br><br></p>
+                        <a href="#">Hospital site</a>
+                    </div>
+                </div>
+                <div class="work-site">
+                    <div class="site-image">
+                        <img src="img/bifree逕ｻ蜒・sample4.png" alt="Corporation site">
+                    </div>
+                    <div class="site-title">
+                        <p>Viewty製薬株式会社<br><br></p>
+                        <a href="#">Corporation site</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="price">
+            <div class="price-list">
+                <div class="price-item">
+                    <h5>LP（1ページ）の制作</h5>
+                    <div class="basic-rate">
+                        <img src="img/bifree逕ｻ蜒・landingpage.png" alt="landingpage">
+                        <p>基本料金</p>
+                        <p class="rate">￥20,000</p>
+                    </div>
+                </div>
+                <div class="price-item">
+                    <h5>ヘッダー画像デザイン</h5>
+                    <div class="basic-rate">
+                        <img src="img/bifree逕ｻ蜒・banner.png" alt="banner">
+                        <p>基本料金</p>
+                        <p class="rate">￥10,000</p>
+                    </div>
+                </div>
+                <div class="price-item">
+                    <h5>Wordpressの構築</h5>
+                    <div class="basic-rate">
+                        <img src="img/bifree逕ｻ蜒・wordpress.png" alt="wordpress">
+                        <p>基本料金</p>
+                        <p class="rate">￥50,000</p>
+                    </div>
+                </div>
+                <div class="price-item">
+                    <h5>ロゴ制作</h5>
+                    <div class="basic-rate">
+                        <img src="img/bifree逕ｻ蜒・group.png" alt="group">
+                        <p>基本料金</p>
+                        <p class="rate">￥20,000</p>
+                    </div>
+                </div>
+            </div>
+            <div class="contact">
+                <div class="contact-flg"></div>
+                <div class="contact-text"><a href="#">お問い合わせはこちら⇀</a></div>
+            </div>
+        </div>
+    </main>
+</html>
