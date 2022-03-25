36fb12dbc7c2c571935f33d815ef2217916ecfe9
bifree <bifree2bfree@gmail.com>
2022-03-26 00:26:10 +0900

    トップページ更新

--- WordPressTheme/assets/js/script.js
+++ WordPressTheme/assets/js/script.js
@@ -2,23 +2,23 @@
 $(function() {
 	if (window.matchMedia( '(min-width: 801px)' ).matches) {
 	$(function(){
-	var scrollStart = $('.main-visual').offset().top; //ページ上部からの距離を取得
+	var scrollStart = $('.main_visual').offset().top; //ページ上部からの距離を取得
 	var scrollEnd = $('.about').offset().top; //ページ上部からの距離を取得
 	var distance = 0;
 	
 	$(document).scroll(function(){
 		distance = $(this).scrollTop(); //スクロールした距離を取得
 	
-		if (scrollStart <= distance) { //スクロール距離が『.catch-copy』の位置を超えたら
-		$('.catch-copy').addClass('fixed'); //class『fixed』を追加
+		if (scrollStart <= distance) { //スクロール距離が『.main_catch』の位置を超えたら
+		$('.main_catch').addClass('fixed'); //class『fixed』を追加
 		} else { //スクロールがページ上部まで戻ったら
-		$('.catch-copy').removeClass('fixed'); //class『fixed』を削除
+		$('.main_catch').removeClass('fixed'); //class『fixed』を削除
 		}
 	
 		if (scrollEnd + 95 <= distance) { //スクロール距離が『.about』の位置を超えたら
-		$('.catch-copy').addClass('stop'); //class『stop』を追加
+		$('.main_catch').addClass('stop'); //class『stop』を追加
 		} else{
-		$('.catch-copy').removeClass('stop'); //『.about』より上部に戻ったらclass『stop』を削除
+		$('.main_catch').removeClass('stop'); //『.about』より上部に戻ったらclass『stop』を削除
 		}
 	});      
 	});
@@ -31,9 +31,9 @@ $(function() {
 		$(document).ready(function(){
 			$(window).on("scroll", function() {
 			if ($(this).scrollTop() > 230) {
-				$(".catch-copy__back").fadeOut("slow");
+				$(".main_catch__back").fadeOut("slow");
 			} else {
-				$(".catch-copy__back").fadeIn("slow");
+				$(".main_catch__back").fadeIn("slow");
 			}
 			});  
 		});
